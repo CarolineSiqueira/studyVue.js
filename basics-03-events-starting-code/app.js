e695e81f23c8722974a2fe2d00fe2d9170bbc7eb
CarolineSiqueira <santos.carolinesiqueira@gmail.com>
2021-05-12 19:27:39 -0300

    Modificadores de Evento

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,9 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:'',
+      confirmedName: ''
     };
   },
   methods: {
+    confirmedInput() {
+      this.confirmedName=this.name;
+    },
+    submitForm() {
+      alert('Submitted!');
+    },
+    setName(event) {
+      this.name = event.target.value;
+    },
     add() {
       this.counter=this.counter + 1;
       //this.counter++;
