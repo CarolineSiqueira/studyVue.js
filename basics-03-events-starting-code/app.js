6a4a99eb5c0e689512991a6e951577b50253f650
CarolineSiqueira <santos.carolinesiqueira@gmail.com>
2021-05-12 10:52:13 -0300

    Trabalhando com argumentos de evento

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -5,12 +5,12 @@ const app = Vue.createApp({
     };
   },
   methods: {
-    add() {
-      this.counter=this.counter + 1;
+    add(num) {
+      this.counter=this.counter + num;
       //this.counter++;
     },
-    remove() {
-      this.counter=this.counter - 1;
+    remove(num) {
+      this.counter=this.counter - num;
       //this.counter--;
     }
   }
